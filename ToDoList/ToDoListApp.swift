b8e5dbbe9266574167aac552a2e94292da496d4d
Akshat2923 <akshat.saladi@gmail.com>
2024-08-05 18:21:15 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Akshat  Saladi on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
