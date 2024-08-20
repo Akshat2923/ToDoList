682597a8accf5155effeb240c430aac8ede13713
Akshat2923 <akshat.saladi@gmail.com>
2024-08-20 01:09:25 -0400

    done!

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -12,11 +12,63 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack{
+                //avatar
+                if let user = viewModel.user{
+                    profile(user: user)
+                } else {
+                    Text("Loading...")
+                }
                 
             }
             .navigationTitle("Profile")
             
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    @ViewBuilder
+    func profile(user: User) -> some View{
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        //info: name, email, member since
+        VStack(alignment: .leading) {
+            HStack{
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+                
+            }
+            .padding()
+            HStack{
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+                
+            }
+            .padding()
+            HStack{
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                
+            }
+            .padding()
+        }
+        
+        
+        Button("Logout"){
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
+        
     }
 }
 
--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -27,6 +27,7 @@ struct ToDoListItemView: View {
                 
             } label : {
                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(.blue)
             }
         }
     }
